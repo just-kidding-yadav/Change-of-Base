12abaa441edb90cf446ef7f1ee441bdee29d0345
Jitender <jkyadav1006@gmail.com>
2020-10-21 23:39:16 +0530

    Handling negative and flawed inputs

--- Change of Base.py
+++ Change of Base.py
@@ -4,24 +4,28 @@ import from_decimal
 import to_decimal
 
 def convert():
-    num1 = int(input("\n" + "Enter the number whose base is to be changed: "))
+    num1 = input("\n" + "Enter a positive number whose base is to be changed: ")
     b1 = int(input("Enter the base of the number: "))
     b2 = int(input("Enter the base of the final result: "))
-
-    for i in range(len(str(num1))):
-        if(int(str(num1)[i]) >= b1):
-            print("\n"+  "The number is not in entered base" + "\n")
-            convert()
-
-    decimal = to_decimal.base_to_dec(num1, b1)
+    if(int(num1)<0):
+        print("The number is negative. Please enter a positive number.")
+        return
+    else:
+        for i in num1:
+            if not(i.isdigit()) and int(i) >= b1:
+                print("Incorrect Base or Number.")
+                return
+    
+    decimal = to_decimal.base_to_dec(int(num1), b1)
     num2 = from_decimal.dec_to_base(decimal, b2)
 
     print("\n" + "The number in final base is: " + str(num2) + "\n")
-
     return
 
 def quit():
     pass
+
+print("Choose a base: 2-10.")
 
 y = "c"
 while(y == "c"):
